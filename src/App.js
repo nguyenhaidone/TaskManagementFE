d93aa52f88cdbe2f7f0bda47c79b52584763dbad
Nguyen Hai Dang <dang682000@gmail.com>
2022-02-19 21:13:47 +0700

    initial project

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <nav className="navbar option">Option bar</nav>
+      <nav className="navbar board">Board bar</nav>
+      <nav className="workspace">
+        <div className="columns">
+          <header>Brainstorm </header>
+          <ul> 
+            <li>
+              <img src="https://o.rada.vn/data/image/2021/08/21/Yae-Genshin-Impact-7.jpg" alt="yae miko"  />
+              TITLE: YAE MIKO 
+            </li>
+            <li>Add everything you want </li>
+            <li>Add everything you want </li>
+            <li>Add everything you want </li>
+            <li>Add everything you want </li>
+            <li>Add everything you want </li>
+            <li>Add everything you want </li>
+            <li>Add everything you want </li>
+            <li>Add everything you want </li>
+            <li>Add everything you want </li>
+            <li>Add everything you want </li>
+            <li>Add everything you want </li>
+            <li>Add everything you want </li>
+            <li>Add everything you want </li>
+            <li>Add everything you want </li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
