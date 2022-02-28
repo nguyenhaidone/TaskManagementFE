f4670f6b0b038144912e9c505e617c93ef6820f6
Nguyen Hai Dang <dang682000@gmail.com>
2022-02-28 09:25:10 +0700

    chore: update style scss for column item

--- src/App.js
+++ src/App.js
@@ -17,7 +17,115 @@ function App() {
             <li>Add everything you want </li>
             <li>Add everything you want </li>
             <li>Add everything you want </li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="columns">
+          <header>Brainstorm </header>
+          <ul> 
+            <li>
+              <img src="https://o.rada.vn/data/image/2021/08/21/Yae-Genshin-Impact-7.jpg" alt="yae miko"  />
+              TITLE: YAE MIKO 
+            </li>
             <li>Add everything you want </li>
+            <li>Add everything you want </li>
+            <li>Add everything you want </li>
+            <li>Add everything you want </li>
+            <li>Add everything you want </li>
+            <li>Add everything you want </li>
+            <li>Add everything you want </li>
+            <li>Add everything you want </li>
+            <li>Add everything you want </li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="columns">
+          <header>Brainstorm </header>
+          <ul> 
+            <li>
+              <img src="https://o.rada.vn/data/image/2021/08/21/Yae-Genshin-Impact-7.jpg" alt="yae miko"  />
+              TITLE: YAE MIKO 
+            </li>
+            <li>Add everything you want </li>
+            <li>Add everything you want </li>
+            <li>Add everything you want </li>
+            <li>Add everything you want </li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="columns">
+          <header>Brainstorm </header>
+          <ul> 
+            <li>
+              <img src="https://o.rada.vn/data/image/2021/08/21/Yae-Genshin-Impact-7.jpg" alt="yae miko"  />
+              TITLE: YAE MIKO 
+            </li>
+            <li>Add everything you want </li>
+            <li>Add everything you want </li>
+            <li>Add everything you want </li>
+            <li>Add everything you want </li>
+            <li>Add everything you want </li>
+            <li>Add everything you want </li>
+            <li>Add everything you want </li>
+            <li>Add everything you want </li>
+            <li>Add everything you want </li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="columns">
+          <header>Brainstorm </header>
+          <ul> 
+            <li>
+              <img src="https://o.rada.vn/data/image/2021/08/21/Yae-Genshin-Impact-7.jpg" alt="yae miko"  />
+              TITLE: YAE MIKO 
+            </li>
+            <li>Add everything you want </li>
+            <li>Add everything you want </li>
+            <li>Add everything you want </li>
+            <li>Add everything you want </li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="columns">
+          <header>Brainstorm </header>
+          <ul> 
+            <li>
+              <img src="https://o.rada.vn/data/image/2021/08/21/Yae-Genshin-Impact-7.jpg" alt="yae miko"  />
+              TITLE: YAE MIKO 
+            </li>
+            <li>Add everything you want </li>
+            <li>Add everything you want </li>
+            <li>Add everything you want </li>
+            <li>Add everything you want </li>
+            <li>Add everything you want </li>
+            <li>Add everything you want </li>
+            <li>Add everything you want </li>
+            <li>Add everything you want </li>
+            <li>Add everything you want </li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="columns">
+          <header>Brainstorm </header>
+          <ul> 
+            <li>
+              <img src="https://o.rada.vn/data/image/2021/08/21/Yae-Genshin-Impact-7.jpg" alt="yae miko"  />
+              TITLE: YAE MIKO 
+            </li>
+            <li>Add everything you want </li>
+            <li>Add everything you want </li>
+            <li>Add everything you want </li>
+            <li>Add everything you want </li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="columns">
+          <header>Brainstorm </header>
+          <ul> 
+            <li>
+              <img src="https://o.rada.vn/data/image/2021/08/21/Yae-Genshin-Impact-7.jpg" alt="yae miko"  />
+              TITLE: YAE MIKO 
+            </li>
             <li>Add everything you want </li>
             <li>Add everything you want </li>
             <li>Add everything you want </li>
