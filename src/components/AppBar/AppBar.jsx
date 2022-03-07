e8e36cb180ea9c8192709609f679ed242ccbc962
Nguyen Hai Dang <dang682000@gmail.com>
2022-03-07 08:58:59 +0700

    chore/refactor

---
+++ src/components/AppBar/AppBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./AppBar.scss";
+
+const AppBar = () =>{
+    return <>
+      <nav className="navbar-app">Option bar</nav>
+    </>
+}
+
+export default React.memo(AppBar);
