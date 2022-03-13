6543b389b1290d0bcefed0bf8829a2f2c5bc4c59
Nguyen Hai Dang <dang682000@gmail.com>
2022-03-13 22:29:08 +0700

    task/intergrate-multilanguage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.scss';
-import React from 'react';
-import AppBar from 'components/AppBar/AppBar';
-import BoardBar from 'components/BoardBar/BoardBar';
-import BoardContent from 'components/BoardContent/BoardContent';
+import "./App.scss";
+import React from "react";
+import AppBar from "components/AppBar/AppBar";
+import BoardBar from "components/BoardBar/BoardBar";
+import BoardContent from "components/BoardContent/BoardContent";
 
 const App = () => {
+
   return (
     <div className="App">
       <AppBar/>
@@ -12,6 +13,6 @@ const App = () => {
       <BoardContent/>
     </div>
   );
-}
+};
 
 export default React.memo(App);
