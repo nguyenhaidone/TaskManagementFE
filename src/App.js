348f6fe774d8c84099af533a44f20904ae8c3ec8
Nguyen Hai Dang <dang682000@gmail.com>
2022-03-29 16:28:44 +0700

    task/login-screen

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import React from "react";
 import AppBar from "components/AppBar/AppBar";
 import BoardBar from "components/BoardBar/BoardBar";
 import BoardContent from "components/BoardContent/BoardContent";
+import Login from "components/Login/Login";
+import Register from "components/Register/Register";
 
 const App = () => {
-
   return (
     <div className="App">
-      <AppBar/>
+      {/* <AppBar/>
       <BoardBar/>
       <BoardContent/>
+      <Login/> */}
+      <Register />
     </div>
   );
 };
